ed2a39cb52b938ff5e48b39900d0ba34fa7c4537
Nicolas Tobar <tobar.n@husky.neu.edu>
2020-11-09 16:49:13 -0500

    adding react router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
