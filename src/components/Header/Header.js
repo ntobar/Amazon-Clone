3556b32fbd5c779eb6126f2c2f04bdcef5d52d28
Nicolas Tobar <tobar.n@husky.neu.edu>
2020-11-10 10:29:10 -0500

    basket count works

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
+import { useStateValue } from "../../StateProvider";
 
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 function Header() {
+  const [{ basket }, dispatch] = useStateValue();
+
   return (
     <div className="header">
       <Link to="/">
@@ -38,7 +41,9 @@ function Header() {
         <Link to="/checkout">
           <div className="header_optionBasket">
             <ShoppingBasketIcon />
-            <span className="header_optionLineTwo header_basketCount">0</span>
+            <span className="header_optionLineTwo header_basketCount">
+              {basket?.length}
+            </span>
           </div>
         </Link>
       </div>
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,7 +1,25 @@
 import React from "react";
 import "./Product.css";
+import { useStateValue } from "../../StateProvider";
 
 function Product({ id, title, price, image, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  console.log("this is the basket -> ", basket);
+
+  const addToBasket = () => {
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+
   return (
     <div className="product">
       <div className="product_info">
@@ -21,7 +39,7 @@ function Product({ id, title, price, image, rating }) {
 
       <img src={image} alt="" />
 
-      <button>Add to Cart</button>
+      <button onClick={addToBasket}>Add to Cart</button>
     </div>
   );
 }
