28becdc30131cec48d4a18fbea1f5bc0baa4abff
Nicolas Tobar <tobar.n@husky.neu.edu>
2020-11-10 17:51:54 -0500

    login functionality starting

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
+import { Link } from "react-router-dom";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="login">
-      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png" />
+      <Link to="/">
+        <img
+          className="login_logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+        />
+      </Link>
+
+      <div className="login_container">
+        <h1>Sign-in</h1>
+        <form>
+          <h5>Email</h5>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <h5>Password</h5>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button className="login_signinButton">Sign In</button>
+        </form>
+        <p>
+          By signing-in you are agreeing to Apollo's Conditions of Use and Terms
+          of Service. Please see our Privacy Notice and Terms of Service.
+        </p>
+        <button className="login_registerButton">
+          Create your Apollo account
+        </button>
+      </div>
     </div>
   );
 }
