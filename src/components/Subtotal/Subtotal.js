b615593131f240f0de4860daa48b58a173ead242
Nicolas Tobar <tobar.n@husky.neu.edu>
2020-11-10 10:48:43 -0500

    working on subtotal value in checkout

--- src/components/Subtotal/Subtotal.js
+++ src/components/Subtotal/Subtotal.js
@@ -1,16 +1,26 @@
 import React from "react";
 import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
+import { useStateValue } from "../../StateProvider";
 
 function Subtotal() {
+  const [{ basket }, dispatch] = useStateValue();
+  let subtotalValue = 0;
+
+  function getSubtotal(item) {
+    subtotalValue += item.price;
+    console.log(`current subtotal: ${subtotalValue}`);
+    return subtotalValue;
+  }
+
   return (
     <div className="subtotal">
       <CurrencyFormat
         renderText={(value) => (
           <>
             <p>
-              Subtotal (0 items):
-              <strong>0</strong>
+              Subtotal ({basket.length} items):
+              <strong>{basket.map(getSubtotal)}</strong>
             </p>
             <small className="subtotal_gift">
               <input type="checkbox" /> This order contains a gift{" "}
