8fd547ab47ee311b7ef1f1a47d3a8ecaccfb3397
Nicolas Tobar <tobar.n@husky.neu.edu>
2020-11-15 18:27:48 -0500

    creating payment component

--- src/components/Payment/Payment.js
+++ src/components/Payment/Payment.js
@@ -1,2 +1,9 @@
 import React from "react";
 import "./Payment.css";
+import { useStateValue } from "../../StateProvider";
+
+function Payment() {
+  const [{ basket, user }, dispatch] = useStateValue();
+
+  return <div></div>;
+}
