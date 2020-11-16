ce248d29d68bcb4e97dbd1a4febfd668a9e529b9
Nicolas Tobar <tobar.n@husky.neu.edu>
2020-11-16 18:48:35 -0500

    typed in secret key

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,9 @@
 const functions = require("firebase-functions");
 const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")(
+  "sk_test_51HTxCVDTDxndnwTNF7Q6cFqasqh8M98K1I5KvqTYpPs1GdTEjMZ6Pv7uRS9RLU7HfgNh54Nnv0a1IqDttrNEpAE400tSRzX8Ts"
+);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
